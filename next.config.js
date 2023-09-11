de847e299e2455edb78be44d8cccc9b0f0aa0996
Erick Alonso <alcuadradoalonso@gmail.com>
2023-09-11 06:25:11 -0600

    Changing the next configuration to avoid CORS

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://url-de-tu-api/:path*',
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
